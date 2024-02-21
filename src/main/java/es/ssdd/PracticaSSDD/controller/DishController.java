85f2bba2d1716d1d7c4d17c02dd5622c5498fa7b
Eva <alcorcon03@gmail.com>
2024-02-21 13:31:19 +0100

    Vuelta controllers

--- src/main/java/es/ssdd/PracticaSSDD/controller/DishController.java
+++ src/main/java/es/ssdd/PracticaSSDD/controller/DishController.java
@@ -1,22 +1,31 @@
 package es.ssdd.PracticaSSDD.controller;
 
 import es.ssdd.PracticaSSDD.data.Dish;
-import es.ssdd.PracticaSSDD.service.DishService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class DishController {
 
-    @Autowired
-    private DishService dishService;
+    private final ConcurrentMap<Long, Dish> dishes = new ConcurrentHashMap<>();
+    private final AtomicLong counter = new AtomicLong();
+
+    public String createDish(Dish dish) {
+        Long id = counter.incrementAndGet();
+        dish.setId(id);
+        dishes.put(id, dish);
+        return "Plato creado con éxito";
+    }
 
     @GetMapping("/dishes")
     public String listDishes(Model model) {
-        model.addAttribute("dishes", dishService.getDishes());
+        model.addAttribute("dishes", dishes.values());
         return "dishes";
     }
 
@@ -28,19 +37,10 @@ public class DishController {
 
     @PostMapping("add-dish")
     public String processFormAddDishes(Dish dish, Model model) {
-        String success = dishService.createDish(dish);
+        String success = createDish(dish);
         model.addAttribute("success", success);
-        return "redirect:/index";
+        return "redirect:/";
     }
 
-    /* Idea Fer para el postMapping del dish y que redirija a index*/
-   /* @PostMapping("/index/add-dish")
-    public String processFormAddDishes(Dish dish) {
-        long id = counter.incrementAndGet();
-        dish.setId(id);
-        dishes.put(id, dish);
-        String success = dishService.createDish(dish);
-        model.addAttribute("success", success);
-        return "redirect:/index";
-    } */
+    
 }
