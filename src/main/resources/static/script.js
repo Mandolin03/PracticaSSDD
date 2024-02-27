c8ff23b797a988b97dfffa864436992ac6b42e58
Z4RCO <z3rc0mrm@gmail.com>
2024-02-27 20:41:36 +0100

    added js check to all forms

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,30 +1,48 @@
-function checkForm() {
-    var name = document.forms["dishForm"]["name"].value;
-    var category = document.forms["dishForm"]["category"].value;
-    var price = document.forms["dishForm"]["price"].value;
-    if (name === "") {
+function checkDishForm() {
+    let name = document.forms["dishForm"]["name"].value;
+    let category = document.forms["dishForm"]["category"].value;
+    let price = document.forms["dishForm"]["price"].value;
+    if (name === "" || category === "" || price === "") {
         alert("Todos los campos son obligatorios.");
-        var input = document.getElementById("name");
-        input.setAttribute("placeholder", "Este campo es obligatorio.");
+        document.getElementById("name").setAttribute("placeholder", "Este campo es obligatorio.");
+        document.getElementById("category").setAttribute("placeholder", "Este campo es obligatorio.");
+        document.getElementById("price").setAttribute("placeholder", "Este campo es obligatorio.");
+
         return false;
     }
-    if (category === "") {
-        alert("Todos los campos son obligatorios.");
-        var input = document.getElementById("category");
-        input.setAttribute("placeholder", "Este campo es obligatorio.");
-        return false;
-    }
-    if (price === "") {
-        alert("Todos los campos son obligatorios.");
-        var input = document.getElementById("price");
-        input.setAttribute("placeholder", "Este campo es obligatorio.");
-        return false;
-    }
-    if(isNaN(price)){
-        alert("El precio deber ser un número.");
-        var input = document.getElementById("price");
-        input.setAttribute("placeholder", "Debe ser un número");
+    else if(isNaN(price)){
+        alert("El precio deber ser un real.");
+        let input = document.getElementById("price");
+        input.setAttribute("placeholder", "Debe ser un real");
         input.setAttribute("value", "");
+        return false;
+    }
+    return true;
+}
+function checkIngredientForm(){
+    let name = document.forms["dishForm"]["name"].value;
+    let category = document.forms["dishForm"]["category"].value;
+    if (name === "" || category === "" || price === "") {
+        alert("Todos los campos son obligatorios.");
+        document.getElementById("name").setAttribute("placeholder", "Este campo es obligatorio.");
+        document.getElementById("category").setAttribute("placeholder", "Este campo es obligatorio.");
+        return false;
+    }
+    return true;
+}
+
+function checkTableForm(){
+    let calories = document.forms["tableForm"]["calories"].value
+    let protein = document.forms["tableForm"]["calories"].value
+    let carbohydrates = document.forms["tableForm"]["carbohydrates"].value
+    let fats = document.forms["tableForm"]["fats"].value
+
+    if(calories === "" || protein === "" || carbohydrates === "" || fats === ""){
+        alert("Todos los campos son obligatorios.");
+        document.getElementById("calories").setAttribute("placeholder", "Este campo es obligatorio.");
+        document.getElementById("protein").setAttribute("placeholder", "Este campo es obligatorio.");
+        document.getElementById("carbohydrates").setAttribute("placeholder", "Este campo es obligatorio.");
+        document.getElementById("fats").setAttribute("placeholder", "Este campo es obligatorio.");
         return false;
     }
     return true;
