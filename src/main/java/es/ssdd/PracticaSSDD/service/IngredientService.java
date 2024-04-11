6c1a5300e31cf05aed3537f4aeed9b1164f62284
Z4RCO <z3rc0mrm@gmail.com>
2024-04-11 11:30:45 +0200

    IngredientService changed to use repository

--- src/main/java/es/ssdd/PracticaSSDD/service/IngredientService.java
+++ src/main/java/es/ssdd/PracticaSSDD/service/IngredientService.java
@@ -51,6 +51,7 @@ public class IngredientService {
     }
 
     public boolean removeIngredient(Long id) {
+        if(!ingredientRepository.existsById(id)) return false;
         ingredientRepository.deleteById(id);
         return true;
     }
